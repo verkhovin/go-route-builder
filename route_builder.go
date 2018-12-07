ca57a81d38ce21c3f5683be876700d6920307e4e
Nikita Verkhovin <verkhovin13@gmail.com>
2018-12-07 23:46:24 +0300

    implement add method for routing tree

--- route_builder.go
+++ route_builder.go
@@ -12,8 +12,8 @@ type RouteBuilder struct {
 func NewBuilder() *RouteBuilder {
 	coreNode := RoutingTreeNode{
 		children: map[string]*RoutingTreeNode{},
-		name: "/",
-		methods: map[string]http.Handler{},
+		name:     "/",
+		methods:  map[string]http.Handler{},
 	}
 	return &RouteBuilder{RoutingTree{&coreNode}}
 }
@@ -24,20 +24,31 @@ type RoutingTree struct {
 
 type RoutingTreeNode struct {
 	children map[string]*RoutingTreeNode
-	name string
-	methods map[string]http.Handler
+	name     string
+	methods  map[string]http.Handler
 }
 
 func (t RoutingTree) add(pattern string, method string, handler http.Handler) {
-	path := strings.Split(pattern, "/")
+	path := strings.FieldsFunc(pattern, splitFunc)
 	node := t.core
 	for _, value := range path {
-		previousNode := node
-		node = node.children[value]
-		if node == nil {
-			createChild(previousNode, value)
-		} else {
-			
+		child := node.children[value]
+		if child == nil {
+			child = newNode(value)
+			node.children[value] = child
 		}
+		node = child
 	}
+	node.methods[method] = handler
+}
+
+func newNode(name string) *RoutingTreeNode {
+	return &RoutingTreeNode{name: name,
+		children: map[string]*RoutingTreeNode{},
+		methods:  map[string]http.Handler{},
+	}
+}
+
+func splitFunc(char rune) bool {
+	return char == '/'
 }
