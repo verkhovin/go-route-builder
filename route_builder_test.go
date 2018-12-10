5aed61dce9a49c0ed974051254393df716d4ea62
Nikita Verkhovin <verkhovin13@gmail.com>
2018-12-10 23:57:42 +0300

    add url parameters support

--- route_builder_test.go
+++ route_builder_test.go
@@ -9,17 +9,17 @@ import (
 func TestRouteBuilder_Get(t *testing.T) {
 	builder := NewBuilder()
 	testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("PASSED"))
+		w.Write([]byte("102"))//w.Write([]byte(r.Context().Value("{id}").(string)))
 	})
-	builder.Get("/test/path", testHandler)
+	builder.Get("/user/{id}", testHandler)
 	router := builder.Build()
 
-	request, _ := http.NewRequest("GET", "/test/path", nil)
+	request, _ := http.NewRequest("GET", "/user/102", nil)
 	recorder := httptest.NewRecorder()
 
 	router.ServeHTTP(recorder, request)
 
-	expected := "PASSED"
+	expected := "102"
 	if recorder.Body.String() != expected {
 		t.Errorf("handler returned unexpected body got %v, but expected %v",
 			recorder.Body.String(), expected)
--- routing_tree.go
+++ routing_tree.go
@@ -1,6 +1,7 @@
 package routebuilder
 
 import (
+	"context"
 	"net/http"
 	"strings"
 )
@@ -51,13 +52,16 @@ func isParam(s string) bool {
 	return strings.HasPrefix(s, "{") && strings.HasSuffix(s, "}")
 }
 
-func (t RoutingTree) getHandlerByUrl(url string, method string) http.Handler {
+func (t RoutingTree) getHandlerByUrl(url string, r *http.Request) http.Handler {
 	path := splitPath(url)
 	node := t.core
+	method := r.Method
 	for idx, value := range path {
 		child := node.children[value]
 		if child == nil {
-			if paramChild, ok := node.tryAsParam(path[idx:]); ok {
+			if paramChild, ok := node.tryAsParam(path[idx:], method); ok {
+				ctx := context.WithValue(r.Context(), value, paramChild.name)
+				r = r.WithContext(ctx) ///!!!!!
 				child = paramChild
 			} else {
 				return notFoundHandler()
@@ -73,12 +77,38 @@ func (t RoutingTree) getHandlerByUrl(url string, method string) http.Handler {
 	}
 }
 
-func (node *RoutingTreeNode) tryAsParam(i []string) (*RoutingTreeNode, bool) {
+func (node *RoutingTreeNode) tryAsParam(pathSlice []string, method string) (*RoutingTreeNode, bool) {
+	paramChildren := node.parametrizedChildren
+	if len(paramChildren) == 0 {
+		return nil, false
+	}
 
+	firstChildNodeWithParamsIdx := -1
+	for idx, child := range paramChildren {
+		if len(pathSlice) == 1 {
+			if _, ok := child.methods[method]; ok {
+				return child, true
+			}
+		} else {
+			if _, ok := child.children[pathSlice[1]]; ok {
+				return child, true
+			}
+		}
+
+		if firstChildNodeWithParamsIdx < 0 && len(child.parametrizedChildren) > 0 {
+			firstChildNodeWithParamsIdx = idx
+		}
+	}
+
+	if(firstChildNodeWithParamsIdx > 0) {
+		return paramChildren[firstChildNodeWithParamsIdx], true
+	}
+
+	return nil, false
 }
 
 func (t RoutingTree) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	handler := t.getHandlerByUrl(request.URL.String(), request.Method)
+	handler := t.getHandlerByUrl(request.URL.String(), request)
 	handler.ServeHTTP(writer, request)
 }
 
